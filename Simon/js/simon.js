7da82c1bd9b7abbd9a2ebb980aba7cc8abcad637
GeOnE-357 <emedarias@gmail.com>
2019-05-05 11:21:49 -0300

    Viendo las funciones de tiempo y random

---
+++ Simon/js/simon.js
@@ -0,0 +1,19 @@
+var a=document.getElementsByTagName("article");
+var bot=[]
+for(i=0; i<a.length;i++)
+	{
+		bot.push(a[i]);
+	}
+console.log(bot);
+var item = bot[Math.floor(Math.random()*bot.length)];//Elemento conseguido al azar del vector bot.
+var sec=[]
+sec.push(item);
+console.log(sec);
+//sec.push(bot[0]); Agrega un elemento al vector.
+//sec.push(bot[1]);
+//sec.push(bot[2]);
+//sec.push(bot[3]);
+//tiempo=setTimeout(function(){ alert("Hello"); }, 3000); define un tiempo limite.
+//clearTimeout(tiempo) Anula el tiempo de de la funcion setTimeout.
+
+console.log(item);
