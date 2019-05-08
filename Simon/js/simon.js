c4a0a1520c096677361b1ea21a67ad3ce2d148c7
GeOnE-357 <emedarias@gmail.com>
2019-05-08 13:52:37 -0300

    Comienzo de las funciones.

--- Simon/js/simon.js
+++ Simon/js/simon.js
@@ -1,14 +1,44 @@
 var a=document.getElementsByTagName("article");
-var bot=[]
+var bot=[] //Vector de los botones vacio.
+var secuencia=[] //Vector de la secuencia generada por el programa.
+var secuencia2=[] //Vector de la secuencia generada por el programa.
 for(i=0; i<a.length;i++)
 	{
-		bot.push(a[i]);
+		bot.push(a[i]);//Agregamos los botones al vector.
+		a[i].onclick=marcar;//Agrega el evento on click a los articulos.		
 	}
-console.log(bot);
-var item = bot[Math.floor(Math.random()*bot.length)];//Elemento conseguido al azar del vector bot.
-var sec=[]
-sec.push(item);
-console.log(sec);
-//sec.push(bot[0]); Agrega un elemento al vector.
-tiempo=setTimeout(function(){ alert("Hello"); }, 3000); //define un tiempo limite.
+
+
+
+function secuencia()
+	{
+		var item = bot[Math.floor(Math.random()*bot.length)];//Elemento conseguido al azar del vector bot.
+		secuencia.push(item); //Agregamos el boton elegido al azar en la secuencia.	
+	}
+
+tiempo=setTimeout(ganar, 3000);
+function marcar(event)
+	{
+		var acc=window.event || event;
+		var pos = acc.target || acc.srcElement;
+		var p=document.getElementById(pos.id);
+		ganar(p);		
+	}
+
+function ganar(p)
+	{
+		if (secuencia2[0]==p)
+			{
+				alert("Click correcto.");
+				secuencia.pop(0);
+				console.log(secuencia);
+				clearTimeout(tiempo);
+			}
+		else
+			{
+				alert("Perdiste.");
+				clearTimeout(tiempo);
+			}
+	}
+//tiempo=setTimeout(function(){ alert("Hello"); }, 3000); //define un tiempo limite.
 //clearTimeout(tiempo);//Anula el tiempo de de la funcion setTimeout.
