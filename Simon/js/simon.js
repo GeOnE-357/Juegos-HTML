1448cf57d8c32686539774e20c2eed64620a949c
GeOnE-357 <emedarias@gmail.com>
2019-05-08 22:19:38 -0300

    Arreglado

--- Simon/js/simon.js
+++ Simon/js/simon.js
@@ -1,8 +1,14 @@
+function inicio()
+	{
+
+	
+
 var a=document.getElementsByTagName("article");
 var bot=[]; //Vector de los botones vacio.
 var sec=[]; //Vector de la secuencia generada por el programa.
 var sec2=[];
 var puntos=0;
+var temp=0;
 var tiempo;
 for(i=0; i<a.length;i++)
 	{
@@ -19,62 +25,77 @@ function inicio()
 
 function copia()
 	{
-		for(i=0; i<sec.length;i++)
+		for(i=temp; i<sec.length;i++)
 			{
-				sec2.push(sec[i]);//Vector de la secuencia generada por el programa.					
-			}
-		for(i=0; i<sec.length;i++)
-			{
-				if(sec[i].id=="bot1")
+				if(sec[i]!=sec2[i])
 					{
-						console.log("Rojo");
-						sec[i].style.backgroundColor="rgba(255, 0, 0, 1)"
-						tiempoColor=setInterval(apagar,500,i);
-					}
-				if(sec[i].id=="bot2")
-					{
-						console.log("Azul");
-						sec[i].style.backgroundColor="rgba(0, 0, 255, 1)";		
-						tiempoColor=setInterval(apagar,500,i);
-					}
-				if(sec[i].id=="bot3")
-					{
-						console.log("Amarillo");
-						sec[i].style.backgroundColor="rgba(255, 255, 0, 1)";
-						tiempoColor=setInterval(apagar,500,i);
-					}
-				if(sec[i].id=="bot4")
-					{
-						console.log("Verder");
-						sec[i].style.backgroundColor="rgba(0, 255, 0, 1)";	
-						tiempoColor=setInterval(apagar,500,i);
-					}
-			}
+						sec2.push(sec[i]);//Vector de la secuencia generada por el programa.												
+						color(i);
+						temp=i;
+						break;
+					}				
+			}	
 		console.log(sec2); 		
-		tiempo=setTimeout(perder, 3000); //Inicia la cuenta del tiempo y si termina, se dispara la funcion perder.
+		if(i>=sec.length)
+				{
+					tiempo=setTimeout(perder, 3000); //Inicia la cuenta del tiempo y si termina, se dispara la funcion perder.			
+				}
+	}
+
+function color(pos)
+	{
+		i=pos;
+		if(sec[i].id=="bot1")
+			{
+				console.log("Rojo");
+				sec[i].style.backgroundColor="rgba(255, 0, 0, 1)"
+				tiempoColor=setTimeout(apagar,500,i);
+			}
+		if(sec[i].id=="bot2")
+			{
+				console.log("Azul");
+				sec[i].style.backgroundColor="rgba(0, 0, 255, 1)";		
+				tiempoColor=setTimeout(apagar,500,i);
+			}
+		if(sec[i].id=="bot3")
+			{
+				console.log("Amarillo");
+				sec[i].style.backgroundColor="rgba(255, 255, 0, 1)";
+				tiempoColor=setTimeout(apagar,500,i);
+			}
+		if(sec[i].id=="bot4")
+			{
+				console.log("Verder");
+				sec[i].style.backgroundColor="rgba(0, 255, 0, 1)";	
+				tiempoColor=setTimeout(apagar,500,i);
+			}
 	}
 
 function apagar(i)
 	{
 		if(sec[i].id=="bot1")
 			{
-				sec[i].style.backgroundColor="rgba(255, 0, 0, 0.4)";
+				sec[i].style.backgroundColor="";
 				clearTimeout(tiempoColor);
+				copia();
 			}
 		if(sec[i].id=="bot2")
 			{
-				sec[i].style.backgroundColor="rgba(0, 0, 255, 0.4)";		
+				sec[i].style.backgroundColor="";		
 				clearTimeout(tiempoColor);
+				copia();
 			}
 		if(sec[i].id=="bot3")
 			{
-				sec[i].style.backgroundColor="rgba(255, 255, 0, 0.4)";
+				sec[i].style.backgroundColor="";
 				clearTimeout(tiempoColor);
+				copia();
 			}
 		if(sec[i].id=="bot4")
 			{
-				sec[i].style.backgroundColor="rgba(0, 255, 0, 0.4)";	
+				sec[i].style.backgroundColor="";	
 				clearTimeout(tiempoColor);
+				copia();
 			}
 	}
 
@@ -87,36 +108,38 @@ function marcar(event)
 	}
 
 function ganar(p)
-	{
-		if (sec2[0]==p)
-			{
-				sec2.shift();
-				console.log(sec2);
-				clearTimeout(tiempo);//Anula el tiempo de de la funcion setTimeout.
-				controlGanar();//Funcion para ver si la secuencia fue terminada o se debe terminar.
-			}
-		else
-			{
-				perder();
-				clearTimeout(tiempo);//Anula el tiempo de de la funcion setTimeout.
-			}
-	}
+		{
+			if (sec2[0]==p)
+				{
+					sec2.shift();
+					console.log(sec2);
+					clearTimeout(tiempo);//Anula el tiempo de de la funcion setTimeout.
+					controlGanar();//Funcion para ver si la secuencia fue terminada o se debe terminar.
+				}
+			else
+				{
+					perder();
+					clearTimeout(tiempo);//Anula el tiempo de de la funcion setTimeout.
+				}
+		}
 
-function controlGanar()
-	{
-		if(sec2.length==0)
-			{
-				puntos=puntos+1;
-				inicio();
+		function controlGanar()
+					{
+			if(sec2.length==0)
+					{
+					puntos=puntos+1;
+					temp=0;
+					inicio();
+				}
+			else
+				{
+					tiempo=setTimeout(perder, 3000);//Re-inicia la cuenta del tiempo y si termina, se dispara la funcion perder.
+				}
 			}
-		else
-			{
-				tiempo=setTimeout(perder, 3000);//Re-inicia la cuenta del tiempo y si termina, se dispara la funcion perder.
-			}
-	}
 
-function perder()
-	{
-		alert("Perdiste");
-		console.log(puntos);
+		function perder()
+			{
+				alert("Perdiste");
+				console.log(puntos);
+			}
 	}
