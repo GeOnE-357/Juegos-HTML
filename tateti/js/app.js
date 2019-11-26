76a416e7e1984f8cb8657cf4a4bb61ead8ffd0b3
GeOnE <emedarias@gmail.com>
2019-11-26 16:06:50 -0300

    Agregado el service worker

--- tateti/js/app.js
+++ tateti/js/app.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator)
 	{
 		/*Empezamo con una promesa de registro.*/
-		navigator.serviceWorker.register('../sw.js')
+		navigator.serviceWorker.register('/tateti/sw.js')
 		.then((reg) => console.log("Service Worker Registrado!", reg))/*Si la promesa es exitosa, se para el parametro "reg" y se imprime el mensaje por consola.*/
 		.catch((err) => console.log("Service Worker No Registrado!", err));/*Sino, se para el parametro "err" y se imprime el mensaje por consola.*/
 	}
