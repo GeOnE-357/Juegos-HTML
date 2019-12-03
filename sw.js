cd09f5e0d12e6a41d1ca34f53c97a805f842936f
GeOnE <emedarias@gmail.com>
2019-12-03 18:14:51 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
+const nombreCache = "sitio-cache"
+const elementos = ["/","/index","/js/accion.js", "/js/app.js", "/css/estilo.css"]
+
 //Instalar el service worker.
-self.addEventListener('install', evt =>{
-	console.log("El service worker esta instalado!");
-});
+self.addEventListener('install', evt =>
+	{
+		//console.log("Service Worker Registrado.");
+		evt.waitUntil(
+				caches.open(nombreCache).then((cache)=>{
+					console.log("Guardando elementos predefinidos en el cache.");
+					cache.addAll(elementos);
+					})
+				)
+	});
+
+//Activar el service worker.
+self.addEventListener('activate', evt =>{ console.log("Service Worker Activado." );});
+
+//Eventos Fetch (fetch request o pedido de busqueda).
+self.addEventListener('fetch', evt =>{console.log("Evento fetch", evt);});
